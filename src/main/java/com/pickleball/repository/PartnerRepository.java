2d0112722a17fe03b83879285017b97a2315d53a
the2sang <the2sang@gmail.com>
2026-02-11 16:29:33 +0900

    사업장 관리자 기능 추가

--- src/main/java/com/pickleball/repository/PartnerRepository.java
+++ src/main/java/com/pickleball/repository/PartnerRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import java.util.Optional;
 
 public interface PartnerRepository extends JpaRepository<Partner, Long> {
+
+  Optional<Partner> findByAccountId(Long accountId);
 
   @Query("""
           SELECT p FROM Partner p
