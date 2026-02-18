8840bb52868f5156557ceb12d27025d45fd5643a
Nico Boco <the2sang@gmail.com>
2026-02-18 14:34:20 +0900

    refresh-expiration-ms 90일로 변경

--- src/main/java/com/pickleball/config/MailTlsConfig.java
+++ src/main/java/com/pickleball/config/MailTlsConfig.java
@@ -30,20 +30,41 @@ public class MailTlsConfig {
 
                 int port = sender.getPort();
                 String protocol = sender.getProtocol();
+                String host = sender.getHost();
 
                 Properties props = sender.getJavaMailProperties();
                 boolean sslEnabled = Boolean.parseBoolean(props.getProperty("mail.smtp.ssl.enable", "false"));
+                boolean startTlsEnabled = Boolean.parseBoolean(props.getProperty("mail.smtp.starttls.enable", "false"));
 
                 boolean implicitTls = port == 465 || "smtps".equalsIgnoreCase(protocol) || sslEnabled;
-                if (!implicitTls) {
+                if (implicitTls) {
+                    props.setProperty("mail.smtp.starttls.enable", "false");
+                    props.setProperty("mail.smtp.starttls.required", "false");
+                    props.setProperty("mail.smtp.ssl.enable", "true");
+                    if (host != null && !host.isBlank() && props.getProperty("mail.smtp.ssl.trust") == null) {
+                        props.setProperty("mail.smtp.ssl.trust", host);
+                    }
+                    log.info("Mail TLS tuned for implicit TLS (protocol={}, port={})", protocol, port);
                     return bean;
                 }
 
-                props.setProperty("mail.smtp.starttls.enable", "false");
-                props.setProperty("mail.smtp.starttls.required", "false");
-                props.setProperty("mail.smtp.ssl.enable", "true");
+                // For explicit STARTTLS (typically 587), ensure SSL and STARTTLS are not both enabled.
+                if (startTlsEnabled && sslEnabled) {
+                    props.setProperty("mail.smtp.ssl.enable", "false");
+                    log.info("Mail TLS normalized for STARTTLS (protocol={}, port={}): disabled implicit SSL", protocol, port);
+                }
 
-                log.info("Mail TLS tuned for implicit TLS (protocol={}, port={})", protocol, port);
+                log.info(
+                        "Mail SMTP effective properties (host={}, protocol={}, port={}, starttls.enable={}, starttls.required={}, ssl.enable={}, ssl.trust={})",
+                        host,
+                        protocol,
+                        port,
+                        props.getProperty("mail.smtp.starttls.enable", "false"),
+                        props.getProperty("mail.smtp.starttls.required", "false"),
+                        props.getProperty("mail.smtp.ssl.enable", "false"),
+                        props.getProperty("mail.smtp.ssl.trust", "")
+                );
+
                 return bean;
             }
         };
